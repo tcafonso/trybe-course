c33e3825236b88e43c0443eb3f309758929400dd
Talita Afonso <talita.afonso24@gmail.com>
2021-11-30 18:27:37 -0300

    Bloco 4 - dia 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/primeiros-passos.js
@@ -0,0 +1,12 @@
+const1 = 10;
+const2 = 5;
+
+let maiorDeDoisNumeros;
+
+if(const1 > const2){
+    maiorDeDoisNumeros = const1;
+} else{
+    maiorDeDoisNumeros = const2;
+}
+
+return maiorDeDoisNumeros;
